d098f33d7150a8dd6287fd2c97ea582abcfdb7a6
Srknkrt <srkn41krt@gmail.com>
2022-03-14 16:26:42 +0300

    DragDrop Güncellemesi
    Drag Drop güncellendi. Game güncellendi. Oyunun kuralları kısmına geçiş yapılmak üzere bırakıldı.

--- 9TasDeneme/Assets/Scripts/DragDrop.cs
+++ 9TasDeneme/Assets/Scripts/DragDrop.cs
@@ -6,21 +6,36 @@ using UnityEngine.EventSystems;
 
 public class DragDrop : MonoBehaviour
 {
+    public GameObject gecerliHareket;
+
     private GameObject selectedObject;
+    private GameObject oldPos;
 
-    private GameObject oldObj;
+    private List<GameObject> nesneler = new List<GameObject>();
+    private GameObject oyunNesnesi;
 
-    private float snapDistance = 1;
+    private float yTas = 0.13f;
+    private float snapDistance = 1.0f;
+    
+    private bool[] yuvalar;
+    private int indis;
 
-    private List<GameObject> oyunNesnesi;
+    private int[,] nesnelerPos = new int[,] { { -9, -9 }, { -9, 0 }, { -9, 9 }, { 0, -9 }, { 0, 9 }, { 9, -9 }, { 9, 0 }, { 9, 9 },
+                                          { -6, -6 }, { -6, 0 }, { -6, 6 }, { 0, -6 }, { 0, 6 }, { 6, -6 }, { 6, 0 }, { 6, 6 },
+                                          { -3, -3 }, { -3, 0 }, { -3, 3 }, { 0, -3 }, { 0, 3 }, { 3, -3 }, { 3, 0 }, { 3, 3 } };
+    private int[,] dogruPos = new int[,] { { -3, 0 }, { 0, 3 }, { 0, -3 }, { 3, 0 } };
 
-    public GameObject gameRules;
+    private string siradakiOyuncu;
+    private int birinciOyuncuTasSayisi = 9;
+    private int ikinciOyuncuTasSayisi = 9;
+    private int kenardakiTasSayisi;
 
-    void Start()
+    private void Start()
     {
-        oldObj = new GameObject();
-        oyunNesnesi = gameRules.GetComponent<GameRules>().nodes;
-        gameRules.GetComponent<GameRules>().OyunNesnesiOlustur();
+        kenardakiTasSayisi = birinciOyuncuTasSayisi + ikinciOyuncuTasSayisi;
+        oldPos = new GameObject("OldPos");
+        yuvalar = new bool[24];
+        siradakiOyuncu = "YesilTas";
     }
 
     private void Update()
@@ -45,12 +60,15 @@ public class DragDrop : MonoBehaviour
         {
             if (hit.collider != null)
             {
-                if (hit.collider.CompareTag("Tas"))
+                if (hit.collider.CompareTag(siradakiOyuncu))
                 {
-                    gameRules.GetComponent<GameRules>().GecerliHareketOlustur();
+                    GecerliHareketOlustur();
                     
                     selectedObject = hit.collider.gameObject;
-                    oldObj.transform.position = selectedObject.transform.position;
+                    oldPos.transform.position = selectedObject.transform.position;
+                    
+                    TasinYeriniBul(selectedObject);
+
                     Cursor.visible = false;
                 }
             }
@@ -64,42 +82,118 @@ public class DragDrop : MonoBehaviour
         if (selectedObject != null)
         {
             worldPosition = FindPosition();
+
             selectedObject.transform.position = new Vector3(worldPosition.x, 0.5f, worldPosition.z);
         }
     }
 
     private void MouseTikiBirakildiginda()
     {
-        string[] yuvaIndisi;
-
-        bool truePos = false;
-
-        float mesafe;
-
         if (selectedObject != null)
         {
-            foreach (GameObject nesne in oyunNesnesi)
+            selectedObject = MesafeHesapla(selectedObject);
+            selectedObject = null;
+            
+            GecerliHareketSil();
+
+            Cursor.visible = true;
+        }
+    }
+
+    private void TasinYeriniBul(GameObject selectedObj)
+    {
+        float mesafe;
+        foreach (GameObject nesne in nesneler)
+        {
+            mesafe = Vector3.Distance(selectedObj.transform.position, nesne.transform.position);
+            if (mesafe <= snapDistance)
             {
-                mesafe = Vector3.Distance(selectedObject.transform.position, nesne.transform.position);
-                if (mesafe <= snapDistance)
+                indis = IndisBul(nesne);
+            }
+        }
+    }
+
+    private GameObject MesafeHesapla(GameObject selectedObj)
+    {
+        bool truePos = false;
+        bool doluMu = false;
+        GameObject n = null;
+        float mesafe;
+        foreach (GameObject nesne in nesneler)
+        {
+            mesafe = Vector3.Distance(selectedObj.transform.position, nesne.transform.position);
+            if (mesafe <= snapDistance)
+            {
+                if (yuvalar[IndisBul(nesne)])
                 {
-                    selectedObject.transform.position = nesne.transform.position;
-
-                    yuvaIndisi = nesne.name.Split(' ');
-                    gameRules.GetComponent<GameRules>().yuvalar[Convert.ToInt32(yuvaIndisi[1])] = 1;
-
+                    doluMu = true;
+                }
+                else
+                {
+                    n = nesne;
+                    yuvalar[indis] = false;
+                    yuvalar[IndisBul(nesne)] = true;
                     truePos = true;
                 }
             }
-            if (!truePos)
-            {
-                selectedObject.transform.position = oldObj.transform.position;
-            }
-
-            gameRules.GetComponent<GameRules>().GecerliHareketSil();
-            selectedObject = null;
-            Cursor.visible = true;
         }
+        if (!truePos || doluMu)
+        {
+            selectedObj.transform.position = oldPos.transform.position;
+        }
+        else
+        {
+            SiradakiOyuncuyuBul();
+            selectedObj.transform.position = n.transform.position;
+            kenardakiTasSayisi -= 1;
+        }
+        return selectedObj;
+    }
+
+    private void SiradakiOyuncuyuBul()
+    {
+        if (siradakiOyuncu == "YesilTas")
+            siradakiOyuncu = "SariTas";
+        else if (siradakiOyuncu == "SariTas")
+            siradakiOyuncu = "YesilTas";
+    }
+
+    private int IndisBul(GameObject nesne)
+    {
+        string[] indis = nesne.name.Split(' ');
+        return Convert.ToInt32(indis[1]);
+    }
+
+    private void GecerliHareketOlustur()
+    {
+        if(kenardakiTasSayisi == 0)
+        {
+        }
+        else
+        {
+            for (int i = 0; i < nesnelerPos.GetLength(0); i++)
+            {
+                for (int j = 0; j < nesnelerPos.GetLength(1) - 1; j++)
+                {
+                    Instantiate(gecerliHareket, new Vector3(nesnelerPos[i, j], yTas, nesnelerPos[i, j + 1]), transform.rotation);
+                    oyunNesnesi = new GameObject("OyunNesnesi " + j + i);
+                    oyunNesnesi.tag = "oyunNesnesi";
+                    oyunNesnesi.transform.position = new Vector3(nesnelerPos[i, j], yTas, nesnelerPos[i, j + 1]);
+                    nesneler.Add(oyunNesnesi);
+                }
+            }
+        }
+    }
+
+    private void GecerliHareketSil()
+    {
+        GameObject[] gecerliH = GameObject.FindGameObjectsWithTag("GecerliHareket");
+        foreach (GameObject gH in gecerliH)
+            GameObject.Destroy(gH);
+        GameObject[] oyunN = GameObject.FindGameObjectsWithTag("oyunNesnesi");
+        foreach (GameObject oN in oyunN)
+            GameObject.Destroy(oN);
+        nesneler.Clear();
     }
 
     private Vector3 FindPosition()
--- 9TasDeneme/Assets/Scripts/Game.cs
+++ 9TasDeneme/Assets/Scripts/Game.cs
@@ -12,82 +12,60 @@ public class Game : MonoBehaviour
 
     private float yPiece = 0.05f;
 
+    private int[,] yuvalarPos = new int[,] { { -9, -9 }, { -9, 0 }, { -9, 9 }, { 0, -9 }, { 0, 9 }, { 9, -9 }, { 9, 0 }, { 9, 9 },
+                                          { -6, -6 }, { -6, 0 }, { -6, 6 }, { 0, -6 }, { 0, 6 }, { 6, -6 }, { 6, 0 }, { 6, 6 },
+                                          { -3, -3 }, { -3, 0 }, { -3, 3 }, { 0, -3 }, { 0, 3 }, { 3, -3 }, { 3, 0 }, { 3, 3 } };
+
+    private float[,] dikCizgilerPos = new float[,] { { -9, -7.5f }, { -9, -4.5f }, { -9, -1.5f }, { -9, 1.5f }, { -9, 4.5f }, { -9, 7.5f },
+                                                  { -6, -4.5f }, { -6, -1.5f }, { -6, 1.5f }, { -6, 4.5f },
+                                                  { -3, -1.5f }, { -3, 1.5f }, { 3, -1.5f }, { 3, 1.5f },
+                                                  { 0, -7.5f }, { 0, -4.5f }, { 0, 4.5f }, { 0, 7.5f },
+                                                  { 6, -4.5f }, { 6, -1.5f }, { 6, 1.5f }, { 6, 4.5f },
+                                                  { 9, -7.5f }, { 9, -4.5f }, { 9, -1.5f }, { 9, 1.5f }, { 9, 4.5f }, { 9, 7.5f } };
+
+    private float[,] yanCizgilerPos = new float[,] { { -7.5f, -9 }, { -4.5f, -9 }, { -1.5f, -9 }, { 1.5f, -9 }, { 4.5f, -9 }, { 7.5f, -9 },
+                                                  { -4.5f, -6 }, { -1.5f, -6 }, { 1.5f, -6 }, { 4.5f, -6 },
+                                                  { -1.5f, -3 }, { 1.5f, -3 }, { -1.5f, 3 }, { 1.5f, 3 },
+                                                  { -7.5f, 0 }, { -4.5f, 0 }, { 4.5f, 0 }, { 7.5f, 0 },
+                                                  { -4.5f, 6 }, { -1.5f, 6 }, { 1.5f, 6 }, { 4.5f, 6 },
+                                                  { -7.5f, 9 }, { -4.5f, 9 }, { -1.5f, 9 }, { 1.5f, 9 }, { 4.5f, 9 }, { 7.5f, 9 } };
+
     void Start()
     {
-        Debug.Log("Hello World!");
-
+        YuvalariYerlestir();
+        CizgileriYerlestir();
         TaslariYerlestir();
+    }
 
-        CizgileriVeYuvalariYerlestir();
+    private void YuvalariYerlestir()
+    {
+        for (int i = 0; i < yuvalarPos.GetLength(0); i++)
+        {
+            for (int j = 0; j < yuvalarPos.GetLength(1) - 1; j++)
+            {
+                Instantiate(yuva, new Vector3(yuvalarPos[i, j], yPiece, yuvalarPos[i, j + 1]), transform.rotation);
+            }
+        }
+    }
+
+    private void CizgileriYerlestir()
+    {
+        for (int i = 0; i < dikCizgilerPos.GetLength(0); i++)
+        {
+            for (int j = 0; j < dikCizgilerPos.GetLength(1) - 1; j++)
+            {
+                Instantiate(dikCizgi, new Vector3(dikCizgilerPos[i, j], yPiece, dikCizgilerPos[i, j + 1]), transform.rotation);
+                Instantiate(yanCizgi, new Vector3(yanCizgilerPos[i, j], yPiece, yanCizgilerPos[i, j + 1]), transform.rotation);
+            }
+        }
     }
 
     private void TaslariYerlestir()
     {
-        //Sari ve Yesil taslar yerlestirildi.
         for (int i = 0; i < 9; i++)
         {
-            Instantiate(YesilTas, new Vector3(-12, yPiece, 6), transform.rotation);
-            Instantiate(SariTas, new Vector3(-12, yPiece, -6), transform.rotation);
-        }
-    }
-
-    private void CizgileriVeYuvalariYerlestir()
-    {
-        float k = 1.5f;
-
-        for (int i = -9; i <= 9; i += 3)
-        {
-            for (int j = -9; j <= 9; j += 3)
-            {
-                //Yuvalar yerlestirildi.
-                if(i != 0)
-                {
-                    if ((j == i || j == -i || j == 0))
-                    {
-                        Instantiate(yuva, new Vector3(i, yPiece, j), transform.rotation);
-                    }
-                }
-                else
-                {
-                    if (j == 3 || j == 6 || j == 9 || j == -3 || j == -6 || j == -9)
-                    {
-                        Instantiate(yuva, new Vector3(i, yPiece, j), transform.rotation);
-                    }
-                }
-                //Cizgiler Yerlestirildi.
-                if(j != 9)
-                {
-                    if (i == -9 || i == 9)
-                    {
-                        Instantiate(dikCizgi, new Vector3(i, yPiece, j + k), transform.rotation);
-                        Instantiate(yanCizgi, new Vector3(j + k, yPiece, i), transform.rotation);
-                    }
-                    else if (i == -6 || i == 6)
-                    {
-                        if (j + k == -4.5 || j + k == -1.5 || j + k == 1.5 || j + k == 4.5)
-                        {
-                            Instantiate(dikCizgi, new Vector3(i, yPiece, j + k), transform.rotation);
-                            Instantiate(yanCizgi, new Vector3(j + k, yPiece, i), transform.rotation);
-                        }
-                    }
-                    else if (i == -3 || i == 3)
-                    {
-                        if (j + k == -1.5 || j + k == 1.5)
-                        {
-                            Instantiate(dikCizgi, new Vector3(i, yPiece, j + k), transform.rotation);
-                            Instantiate(yanCizgi, new Vector3(j + k, yPiece, i), transform.rotation);
-                        }
-                    }
-                    else if (i == 0)
-                    {
-                        if (j + k == -7.5 || j + k == -4.5 || j + k == 4.5 || j + k == 7.5)
-                        {
-                            Instantiate(dikCizgi, new Vector3(i, yPiece, j + k), transform.rotation);
-                            Instantiate(yanCizgi, new Vector3(j + k, yPiece, i), transform.rotation);
-                        }
-                    }
-                }
-            }
+            Instantiate(YesilTas, new Vector3(-12, yPiece, (i * -2) + 8), transform.rotation);
+            Instantiate(SariTas, new Vector3(12, yPiece, (i * -2) + 8), transform.rotation);
         }
     }
 }
